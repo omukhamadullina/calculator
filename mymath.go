c510975e65964b8ae8ca53208ce251892457ad4f
Olga-Muhamadullina <142241700+Olga-Muhamadullina@users.noreply.github.com>
2023-10-09 18:46:52 +0300

    Add files via upload
    Добавлен mymath.go к task1.12.4.1

---
+++ mymath.go
@@ -0,0 +1,19 @@
+package mymath
+
+import (
+	"math"
+)
+
+// Sqrt - функция для вычисления квадратного корня из числа
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
+func Yn(n int, x float64) float64 {
+	return math.Yn(n, x)
+}
